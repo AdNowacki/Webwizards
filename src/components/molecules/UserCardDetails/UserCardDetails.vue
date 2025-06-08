7beb4790f048eba7eaa12ff6b68102aab4152e57
Adam Nowacki <ad.nowacki@gmail.com>
2025-06-08 22:24:32 +0200

    Created UserCardDetails

---
+++ src/components/molecules/UserCardDetails/UserCardDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="user-card-details">
+    {{ content }}
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import Btn from '@/components/atoms/Btn/Btn.vue'
+
+const props = defineProps({
+  content: String,
+});
+</script>
+
+<style lang="scss" scoped>
+@import './style.scss';
+</style>
--- src/components/organisms/UserCard/UserCard.vue
+++ src/components/organisms/UserCard/UserCard.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="user-card">
     <UserCardContact @click="emit('click')" :user="user" />
-    <div class="user-card__details">
-      CCCC
+    <div v-if="user.about" class="user-card__details">
+      <UserCardDetails :content="user.about" />
     </div>
   </div>
 </template>
 
 <script setup>
-import UserCardContact from '@/components/organisms/UserCardContact/UserCardContact.vue';
+import UserCardContact from '@/components/molecules/UserCardContact/UserCardContact.vue';
+import UserCardDetails from '@/components/molecules/UserCardDetails/UserCardDetails.vue';
 
 const props = defineProps({
   user: Object | null,
