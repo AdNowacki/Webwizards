663dd6f299649dd9ca6d8e5c89a3fdc7f9c250c1
Adam Nowacki <ad.nowacki@gmail.com>
2025-06-08 22:41:07 +0200

    Done

--- src/components/molecules/UserCardDetails/UserCardDetails.vue
+++ src/components/molecules/UserCardDetails/UserCardDetails.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="user-card-details">
-    {{ content }}
+    <div v-html="contentResolver"></div>
+    <Btn v-if="!showAllContent" @click="showAllContent = true" variant="primary" label="Rozwiń opis" class="user-card-details__btn" />
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 import Btn from '@/components/atoms/Btn/Btn.vue'
 
 const props = defineProps({
   content: String,
+});
+
+const showAllContent = ref(false);
+
+const contentResolver = computed(() => {
+  const nlToBrContent = props.content.replace(/\n/g, '<br />');
+  return showAllContent.value ? nlToBrContent : nlToBrContent.split('<br />')[0]
 });
 </script>
 
